ab7270bbe7067e343905d2bf62256c5bd38e2277
Elaidzha1940 <eli080293@gmail.com>
2023-04-26 00:21:15 +0700

    added a new File + HomeView

---
+++ Tabbar/Tabbar/Home/HomeView.swift
@@ -0,0 +1,23 @@
+//  /*
+//
+//  Project: Tabbar
+//  File: HomeView.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 26.04.2023
+//
+//  */
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        
+        Text("Home")
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
