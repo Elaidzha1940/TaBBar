f939b9c3d242e1f3a68374364bdb6c7b6ca512cb
Elaidzha1940 <eli080293@gmail.com>
2023-04-25 23:58:03 +0700

    Tabbar

---
+++ Tabbar/Tabbar/TabbarApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabbarApp.swift
+//  Tabbar
+//
+//  Created by Elaidzha Shchukin on 25.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct TabbarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
