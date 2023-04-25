94ed07f689dd0a31a2087f1802155e37cd741ed8
Elaidzha1940 <eli080293@gmail.com>
2023-04-26 00:22:51 +0700

    added a new File + TapeView

---
+++ Tabbar/Tabbar/Tape/TapeView.swift
@@ -0,0 +1,22 @@
+//  /*
+//
+//  Project: Tabbar
+//  File: TapeView.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 26.04.2023
+//
+//  */
+
+import SwiftUI
+
+struct TapeView: View {
+    var body: some View {
+        Text("Tape")
+    }
+}
+
+struct TapeView_Previews: PreviewProvider {
+    static var previews: some View {
+        TapeView()
+    }
+}
