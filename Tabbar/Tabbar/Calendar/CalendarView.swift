aba4fa128756b0aeffd40f7a31e7ecaa9f5f82d3
Elaidzha1940 <eli080293@gmail.com>
2023-04-26 00:25:57 +0700

    added a new File + CalendarView

---
+++ Tabbar/Tabbar/Calendar/CalendarView.swift
@@ -0,0 +1,23 @@
+//  /*
+//
+//  Project: Tabbar
+//  File: CalendarView.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 26.04.2023
+//
+//  */
+
+import SwiftUI
+
+struct CalendarView: View {
+    var body: some View {
+        
+        Text("Calendar")
+    }
+}
+
+struct CalendarView_Previews: PreviewProvider {
+    static var previews: some View {
+        CalendarView()
+    }
+}
